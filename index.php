124d00bc4677a196c18e11091a4751a83ac41bfe
Michu <michus.fer0503@gmail.com>
2021-04-04 12:38:59 -0500

    He agregado un git ignore

--- index.php
+++ index.php
@@ -13,8 +13,20 @@
     }
     include "paginas/".$pagina.".php";
     ////se cierra la conexion
-    $user=null;
+    //$user=null;
     ////////
     include "templates/pie.php";
     
 ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1></h1>
+    <h2></h2>
+</body>
+</html>
